5eda5f53b55bc870e726dbceb62f24d42b2334e1
Hasmukh <hasmukhrathod@gmail.com>
2018-12-10 11:16:15 +0000

    改善：default subject can be override as per requirement.

--- blog_spec.rb
+++ blog_spec.rb
@@ -14,36 +14,36 @@ RSpec.describe Blog do
     ##Rightnow, there are 2 context of is_published? method
     ##1. publish, 2. not publish
     
-    context 'state is published' do
-      it 'if status is true, then it is published.' do
-        blog = described_class.new(status: true)
-        expect(blog).to be_is_published
-      end
-    end
     context 'state is not published' do
       it 'if status is false, then it is not published.' do
         expect(subject).to_not be_is_published
       end
     end
+    
+    context 'state is published' do
+      subject = described_class.new(status: true)
+      it 'if status is true, then it is published.' do
+        expect(subject).to be_is_published
+      end
+    end
   end
-  
 
   describe '#is_blog_popular?' do
     ##we use context when there are paths in method
     ##Here two paths: 1 blog is popular, 2 blog is not popular
     context 'blog is popular' do
+      #default subject is override
+      subject { described_class.new(initial_view_count: 105) }
       it 'if view_count is greater than 100' do
-        blog = described_class.new(initial_view_count: 105)
-        expect(blog).to be_is_blog_popular
+        expect(subject).to be_is_blog_popular
       end
     end
     
     context 'blog is not popular' do
-      it 'if view_count is greater than 100' do
+      subject { described_class.new(initial_view_count: 50) }
+      it 'if view_count is less than 100' do
         expect(subject).to_not be_is_blog_popular
       end
     end
   end
-  
-  
 end
