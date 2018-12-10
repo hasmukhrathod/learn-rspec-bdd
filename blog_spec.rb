47ad825cc77b8c65cfb58f2bb96790bfb05dc978
Hasmukh <hasmukhrathod@gmail.com>
2018-12-10 10:11:42 +0000

    改善：use of described_class

--- blog_spec.rb
+++ blog_spec.rb
@@ -11,14 +11,28 @@ RSpec.describe Blog do
   
   #is blog published? => boolean method
   describe '#is_pubished?' do
-    it 'if status is true, then it is published.' do
+    it 'if status is false, then it is not published.' do
       expect(subject).to_not be_is_published
+    end
+  end
+  
+  describe '#is_pubished?' do
+    it 'if status is true, then it is published.' do
+      blog = described_class.new(status: true)
+      expect(blog).to be_is_published
     end
   end
   
   describe '#is_blog_popular?' do
     it 'if view_count is greater than 100' do
       expect(subject).to_not be_is_blog_popular
+    end
+  end
+  
+  describe '#is_blog_popular?' do
+    it 'if view_count is greater than 100' do
+      blog = described_class.new(initial_view_count: 105)
+      expect(blog).to be_is_blog_popular
     end
   end
   
