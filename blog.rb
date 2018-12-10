9c6f6f947a69aa7540c2632fd0fe1ee5ab76d895
Hasmukh <hasmukhrathod@gmail.com>
2018-12-10 06:32:51 +0000

    Usage of subject with instance method.

--- blog.rb
+++ blog.rb
@@ -1,3 +1,11 @@
 class Blog
   
+  def initialize(initial_view_count: 0)
+    @view_count = initial_view_count
+  end
+  
+  def default_view_count
+    @view_count
+  end
+  
 end
--- blog_spec.rb
+++ blog_spec.rb
@@ -1,3 +1,10 @@
-RSpec.describe do
+require 'rspec'
+require_relative 'blog'
+
+RSpec.describe Blog do
+  #subject {Blog.new}  ##default, rspec does -> so we can use subject.
+  it '#check_default_view_count' do  #what instance method will return?
+    expect(subject.default_view_count).to eq(0)
+  end
   
 end
