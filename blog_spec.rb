c58fd488212675668fa2badf45b31f6a90a90c49
Hasmukh <hasmukhrathod@gmail.com>
2018-12-10 10:32:48 +0000

    改善： Use of context in a method.

--- blog_spec.rb
+++ blog_spec.rb
@@ -11,28 +11,37 @@ RSpec.describe Blog do
   
   #is blog published? => boolean method
   describe '#is_pubished?' do
-    it 'if status is false, then it is not published.' do
-      expect(subject).to_not be_is_published
+    ##Rightnow, there are 2 context of is_published? method
+    ##1. publish, 2. not publish
+    
+    context 'state is published' do
+      it 'if status is true, then it is published.' do
+        blog = described_class.new(status: true)
+        expect(blog).to be_is_published
+      end
+    end
+    context 'state is not published' do
+      it 'if status is false, then it is not published.' do
+        expect(subject).to_not be_is_published
+      end
     end
   end
   
-  describe '#is_pubished?' do
-    it 'if status is true, then it is published.' do
-      blog = described_class.new(status: true)
-      expect(blog).to be_is_published
-    end
-  end
-  
+
   describe '#is_blog_popular?' do
-    it 'if view_count is greater than 100' do
-      expect(subject).to_not be_is_blog_popular
+    ##we use context when there are paths in method
+    ##Here two paths: 1 blog is popular, 2 blog is not popular
+    context 'blog is popular' do
+      it 'if view_count is greater than 100' do
+        blog = described_class.new(initial_view_count: 105)
+        expect(blog).to be_is_blog_popular
+      end
     end
-  end
-  
-  describe '#is_blog_popular?' do
-    it 'if view_count is greater than 100' do
-      blog = described_class.new(initial_view_count: 105)
-      expect(blog).to be_is_blog_popular
+    
+    context 'blog is not popular' do
+      it 'if view_count is greater than 100' do
+        expect(subject).to_not be_is_blog_popular
+      end
     end
   end
   
