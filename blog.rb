78652121db34e39816493076c73b0806a8376340
Hasmukh <hasmukhrathod@gmail.com>
2018-12-10 07:56:04 +0000

    Boolean method added to check the status of the bog

--- blog.rb
+++ blog.rb
@@ -1,11 +1,23 @@
 class Blog
   
-  def initialize(initial_view_count: 0)
+  NO_OF_VIEW_FOR_POPULAR = 100
+  
+  def initialize(initial_view_count: 0, status: false)
     @view_count = initial_view_count
+    @is_published = status
   end
   
   def default_view_count
     @view_count
+  end
+  
+  ##boolean method
+  def is_published?
+    @is_published
+  end
+  
+  def is_blog_popular?
+    @view_count > NO_OF_VIEW_FOR_POPULAR ? true : false
   end
   
 end
--- blog_spec.rb
+++ blog_spec.rb
@@ -3,8 +3,24 @@ require_relative 'blog'
 
 RSpec.describe Blog do
   #subject {Blog.new}  ##default, rspec does -> so we can use subject.
-  it '#check_default_view_count' do  #what instance method will return?
-    expect(subject.default_view_count).to eq(0)
+  describe '#check_default_view_count' do #method name
+    it 'check_default_view_count' do  #what instance method will return?
+      expect(subject.default_view_count).to eq(0)
+    end
+  end  
+  
+  #is blog published? => boolean method
+  describe '#is_pubished?' do
+    it 'if status is true, then it is published.' do
+      expect(subject.is_published?).to eq(false)
+    end
   end
+  
+  describe '#is_blog_popular?' do
+    it 'if view_count is greater than 100' do
+      expect(subject.is_blog_popular?).to eq(false)
+    end
+  end
+  
   
 end
